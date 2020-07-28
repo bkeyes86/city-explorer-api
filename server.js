93448393520b77f25eace8cee904a2a5091db90f
bkeyes86 <byronvkeyes@gmail.com>
2020-07-28 13:53:07 -0500

    almost

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(cors());
 // Route Definition
 app.use(errorHandler);
 app.use('*', notFoundHandler);
+app.get('/', rootHandler);
+app.get('/location', locationHandler);
+app.get('/yelp', restaurantHandler);
+app.get('/weather', weatherHandler);
 
 // Route handlers
 function rootHandler(request, response) {
@@ -21,10 +25,30 @@ function rootHandler(request, response) {
 
 }
 function locationHandler(request, response) {
-  const city = 'seattle';
+  const city = request.query.city;
   const locationData = require('./data/location.json');
   const location = new Location(city, locationData)
   response.status(200).send(location);
+}
+
+function restaurantHandler(request, response) {
+  const restaurantsData = require('./data/restaurants.json');
+  const arrayOfRestaurants = restaurantsData.nearby_restaurants;
+  const restaurantsResults = [];
+  arrayOfRestaurants.forEach(restaurantObj => {
+    restaurantsResults.push(new Restaurant(restaurantObj));
+  });
+  response.send(restaurantsResults)
+}
+
+function weatherHandler(request, response) {
+  const weatherData = require('./data/weather.json');
+  const arrayOfWeatherData = weatherData.data;
+  const weatherResults = [];
+  arrayOfWeatherData.forEach(location => {
+    weatherResults.push(new Weather(location));
+  });
+  response.send(weatherResults)
 }
 
 function notFoundHandler(request, response) {
@@ -32,11 +56,25 @@ function notFoundHandler(request, response) {
 }
 function errorHandler(error, request, response, next) {
   response.status(500).json({error: true, message:error.message});
+
 }
 
 function Location(city, locationData) {
   this.search_query = city;
   this.formatted_query = locationData[0].display_name;
+  this.latitude = parseFloat(locationData[0].lat);
+  this.longitude = parseFloat(locationData[0].lon);
+}
+function Restaurant(obj) {
+  this.name = obj.restaurant.name;
+  this.url = obj.restaurant.url;
+  this.rating = obj.restaurant.user_rating.aggregate_rating;
+  this.price = obj.restaurant.price_range;
+  this.image_url = obj.restaurant.featured_image;
+}
+function Weather(conditions) {
+  this.time = conditions.valid_date;
+  this.forecast = conditions.weather.description;
 }
 
 // App listener
